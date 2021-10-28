ae74053c92d41aa9e36e6f75be41eacb5a8c34cb
Salvatore949 <salvo.mess49@gmail.com>
2021-10-28 15:45:39 +0200

    Primo esercizio

--- primo esercizio/Js/script.js
+++ primo esercizio/Js/script.js
@@ -1,3 +1,9 @@
+// chiedo all'utente di inserire il primo numero
+// chirdo all'utente di inserire il secondo numero
+// Inserisco la condizione: Se il primo numero è più grande del secondo allora verrà visualizzato nel browser
+// Se il secondo numero è maggiore del primo allora sarà quest'ultimo ad essere visualizzato nel browser
+
+
 let elemento = document.getElementById('mio_id');
 
 const numero1 = prompt('Inserisci il primo numero');
